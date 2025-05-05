92a5fe5889510b69040ceb319019d92b64855083
Jathurshaa Singarajah <146446297+Jathu-Jathu@users.noreply.github.com>
2025-05-06 01:28:05 +0530

    Update UserController.java

--- src/main/java/backend/controller/UserController.java
+++ src/main/java/backend/controller/UserController.java
@@ -69,7 +69,7 @@ public class UserController {
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
 
-    //update
+    //update profile
     @PutMapping("/user/{id}")
     UserModel updateProfile(@RequestBody UserModel newUserModel, @PathVariable String id) {
         return userRepository.findById(id)
